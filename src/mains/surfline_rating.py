66b1b0be869babe70883de5ef7ad777952c693da
Matthew King CS2018 <matthew.king.2018@uni.strath.ac.uk>
2022-03-12 16:22:08 +0000

    Added the final 'rating' datasets

--- src/mains/surfline_rating.py
+++ src/mains/surfline_rating.py
@@ -10,7 +10,7 @@ from utils.train_test_utils import *
 
 ENCLOSING_FOLDER = 'rating'
 DATASETS = ['pipeline', 'lorne_point', 'noosa_heads', 'marias_beachfront', 'rocky_point']
-NUM_DATASETS_TO_LOAD = 1
+NUM_DATASETS_TO_LOAD = 2
 TEST_SIZE = 0.2
 K_FOLD_SPLITS = 5
 CONFIGS = {
@@ -29,9 +29,9 @@ CONFIGS = {
 }
 SEEDS = {
     'cnn': get_seed([0, 0, 0, 0, 0], NUM_DATASETS_TO_LOAD),
-    'svm': get_seed([0, 0, 0, 0, 0], NUM_DATASETS_TO_LOAD),
-    'rf': get_seed([0, 0, 0, 0, 0], NUM_DATASETS_TO_LOAD),
-    'knn': get_seed([0, 0, 0, 0, 0], NUM_DATASETS_TO_LOAD)
+    'svm': get_seed([25, 0, 0, 0, 0], NUM_DATASETS_TO_LOAD),
+    'rf': get_seed([34, 0, 0, 0, 0], NUM_DATASETS_TO_LOAD),
+    'knn': get_seed([43, 0, 0, 0, 0], NUM_DATASETS_TO_LOAD)
 }
 
 def main():
@@ -46,18 +46,17 @@ def main():
     knn = RatingKNN()
 
     # Train a single model
-    train_and_test_model(cnn, SEEDS['cnn'], ENCLOSING_FOLDER, DATASETS_TO_LOAD, TEST_SIZE, CONFIGS)
+    # train_and_test_model(cnn, SEEDS['cnn'], ENCLOSING_FOLDER, DATASETS_TO_LOAD, TEST_SIZE, CONFIGS)
 
     # Train multiple models
-    # models_and_seeds = [(cnn, SEEDS['cnn']), (svm, SEEDS['svm']), (rf, SEEDS['rf']),
-    #                     (knn, SEEDS['knn'])]
+    # models_and_seeds = [(cnn, SEEDS['cnn']), (svm, SEEDS['svm']), (rf, SEEDS['rf']), (knn, SEEDS['knn'])]
     # train_and_test_models(models_and_seeds, ENCLOSING_FOLDER, DATASETS_TO_LOAD, TEST_SIZE, CONFIGS)
 
     # Apply k-fold cross validation to one or more models
     # k_fold_cross_validation([cnn], ENCLOSING_FOLDER, DATASETS_TO_LOAD, K_FOLD_SPLITS, TEST_SIZE, CONFIGS)
 
     # Test the Scikit-learn models on a specified number of seeds and find the seed that each model performed best on
-    # find_best_sklearn_seeds([svm, rf, knn], ENCLOSING_FOLDER, DATASETS_TO_LOAD, TEST_SIZE, CONFIGS, 50)
+    find_best_sklearn_seeds([svm, rf, knn], ENCLOSING_FOLDER, DATASETS_TO_LOAD, TEST_SIZE, CONFIGS, 50)
 
 if __name__ == '__main__':
     main()
