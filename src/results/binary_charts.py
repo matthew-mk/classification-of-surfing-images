45a0add119c2427c1731b1d5342fcb49005ecfba
Matthew King CS2018 <matthew.king.2018@uni.strath.ac.uk>
2022-03-15 20:00:07 +0000

    Evaluation phase - Made the plotly bar chart function more modular

--- src/results/binary_charts.py
+++ src/results/binary_charts.py
@@ -1,71 +1,16 @@
 """This module contains functions that create graphs to show the results that were gathered for the binary
 classification models during the evaluation stage of the project. """
 
-import plotly.graph_objects as go
-
-red = '#ff757d'
-blue = '#4cb5f5'
-orange = '#ffb81f'
-purple = '#d472cd'
-
+from utils.helper_utils import create_binary_bar_chart
 
 def magicseaweed_binary_accuracy_chart():
     """Creates a bar chart showing...
     """
-
-    x = ['1', '2', '3', '4', '5']
     cnn_results = [100, 99, 99, 99, 99]
     svm_results = [98, 96, 94, 95, 94]
     rf_results = [97, 95, 93, 94, 94]
     knn_results = [78, 74, 78, 76, 77]
-
-    fig = go.Figure()
-    fig.add_trace(go.Bar(
-        x=x,
-        y=cnn_results,
-        name='CNN',
-        marker_color=red,
-        text=cnn_results,
-        textposition='outside',
-        texttemplate='%{text:.3}'
-    ))
-    fig.add_trace(go.Bar(
-        x=x,
-        y=svm_results,
-        name='SVM',
-        marker_color=blue,
-        text=svm_results,
-        textposition='outside',
-        texttemplate='%{text:.3}'
-    ))
-    fig.add_trace(go.Bar(
-        x=x,
-        y=rf_results,
-        name='RF',
-        marker_color=orange,
-        text=rf_results,
-        textposition='outside',
-        texttemplate='%{text:.3}'
-    ))
-    fig.add_trace(go.Bar(
-        x=x,
-        y=knn_results,
-        name='KNN',
-        marker_color=purple,
-        text=knn_results,
-        textposition='outside',
-        texttemplate='%{text:.3}'
-    ))
-
-    fig.update_xaxes(title_text="Number of locations")
-    fig.update_yaxes(title_text="Accuracy (%)")
-    fig.update_layout(barmode='group',
-                      title={
-                          'text': 'Accuracy of Binary Models on Magicseaweed Datasets (Best Seeds)',
-                          'x': 0.5,
-                          'xanchor': 'center',
-                          'yanchor': 'top'})
-    fig.show()
+    create_binary_bar_chart(cnn_results, svm_results, rf_results, knn_results)
 
 
 def magicseaweed_binary_precision_chart():
--- src/utils/helper_utils.py
+++ src/utils/helper_utils.py
@@ -1,5 +1,6 @@
 """This module contains utility functions that help with various areas of development."""
 
+import plotly.graph_objects as go
 
 def get_channels(color_mode):
     """Returns the number of channels that a color mode has.
@@ -37,3 +38,75 @@ def get_seed(seeds, num_locations):
         raise ValueError(f"The number of locations is too high. Maximum = {len(seeds)}")
 
     return seeds[num_locations - 1]
+
+
+def create_binary_bar_chart(cnn_results, svm_results, rf_results, knn_results):
+    """Creates a bar chart showing how well the binary CNN, SVM, RF, and KNN models performed based on the number of
+    locations images were used from.
+
+    Args:
+        cnn_results (list[int]): The results that the binary CNN model achieved per location.
+        svm_results (list[int]): The results that the binary SVM model achieved per location.
+        rf_results (list[int]): The results that the binary RF model achieved per location.
+        knn_results (list[int]): The results that the binary KNN model achieved per location.
+
+    """
+
+    red = '#ff757d'
+    blue = '#4cb5f5'
+    orange = '#ffb81f'
+    purple = '#d472cd'
+
+    x = ['1', '2', '3', '4', '5']
+    cnn_results = cnn_results
+    svm_results = svm_results
+    rf_results = rf_results
+    knn_results = knn_results
+
+    fig = go.Figure()
+    fig.add_trace(go.Bar(
+        x=x,
+        y=cnn_results,
+        name='CNN',
+        marker_color=red,
+        text=cnn_results,
+        textposition='outside',
+        texttemplate='%{text:.3}'
+    ))
+    fig.add_trace(go.Bar(
+        x=x,
+        y=svm_results,
+        name='SVM',
+        marker_color=blue,
+        text=svm_results,
+        textposition='outside',
+        texttemplate='%{text:.3}'
+    ))
+    fig.add_trace(go.Bar(
+        x=x,
+        y=rf_results,
+        name='RF',
+        marker_color=orange,
+        text=rf_results,
+        textposition='outside',
+        texttemplate='%{text:.3}'
+    ))
+    fig.add_trace(go.Bar(
+        x=x,
+        y=knn_results,
+        name='KNN',
+        marker_color=purple,
+        text=knn_results,
+        textposition='outside',
+        texttemplate='%{text:.3}'
+    ))
+
+    fig.update_xaxes(title_text="Number of locations")
+    fig.update_yaxes(title_text="Accuracy (%)")
+    fig.update_layout(barmode='group',
+                      title={
+                          'text': 'Accuracy of Binary Models on Magicseaweed Datasets (Best Seeds)',
+                          'x': 0.5,
+                          'xanchor': 'center',
+                          'yanchor': 'top'})
+    fig.show()
